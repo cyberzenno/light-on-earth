8bda2a705a893f72b49a1d80e2fbd0e2c0173ead
cyberzenno <zenno.neagu@gmail.com>
2017-02-07 23:54:43 +0000

    added basic year simulation

---
+++ LightOnEarth/Assets/Scripts/RotationAndRevolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationAndRevolution : MonoBehaviour
+{
+    void Update()
+    {
+        var daysPerYear = 10;
+        var secondsPerYear = 10;
+ 
+        // Earth revolution
+        transform.RotateAround(Vector3.zero, Vector3.up, SecondsPerRevolution(secondsPerYear));
+
+        // Earth rotation
+        transform.Rotate(Vector3.up, RotationsPerSecond(daysPerYear / secondsPerYear));
+    }
+
+    float RotationsPerSecond(float rotations)
+    {
+        return -360 * rotations * Time.deltaTime;
+    }
+
+    float SecondsPerRevolution(int seconds)
+    {
+        return (-360 / seconds) * Time.deltaTime;
+    }
+}
